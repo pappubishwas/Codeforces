97179710f2fe372ff516f0f5104d1457026341a8
pappubishwas <pappuovi8@gmail.com>
2023-12-18 00:07:17 +0530

    update

--- C_Largest_Subsequence.cpp
+++ C_Largest_Subsequence.cpp
@@ -1,112 +1,63 @@
-#include<bits/stdc++.h>
-using namespace std;
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template<class T> using pbset=tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update> ;
-template<class T> using pbmultiset=tree<T, null_type, less_equal<T>, rb_tree_tag,tree_order_statistics_node_update> ;
-#define f(i,a,b) for(long long i=a;i<b;i++)
-#define rf(i,a,b) for(long long i=a;i>=b;i--)
-#define ll long long
-#define mp make_pair
-#define pb push_back
-#define vll vector<long long>
-#define vvl vector<vll>
-#define pll pair<ll,ll>
-#define vc vector
-#define fi first
-#define se second
-#define lb lower_bound
-#define sz(a) (ll)(a.size())
-#define remove_unique(x) sort(x.begin(),x.end()); x.erase(unique(x.begin(),x.end()),x.end())
-#define to_lowercase(s) transform(s.begin(), s.end(), s.begin(), ::tolower)
-#define to_uppercase(s) transform(s.begin(), s.end(), s.begin(), ::toupper)
-#define p_q priority_queue
-#define pqmax priority_queue<ll>
-#define pqmin priority_queue<ll,vector<ll>,greater<ll>>
-#define all(x) x.begin(),x.end()
-#define inc(x,start) iota(x.begin(),x.end(),start)
-#define pi  3.141592653589793238
-#define inf  LONG_LONG_MAX
-#define setbits(x) __builtin_popcountll(x)
-#define zerobits(x) __builtin_ctzll(x)
-#define IOS ios_base::sync_with_stdio(0)
-#define tie cin.tie(NULL),cout.tie(NULL) 
-#define endl '\n'
-long long POW(long long a,long long b){return (long long)(pow(a,b)+0.5);}
-long long gcd(long long a,long long b){return __gcd(a,b);}
-long long lcm(long long a,long long b){return (a*b)/__gcd(a,b);}
-long long Ceil(long long a,long long b){return (a+b-1)/b;}
-long long Sqrt(long long x){ long long y=sqrt(x)+5;while(y*y>x)y--;return y;}
-long long sq(long long x){return (1ll*x*x);}
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x) 
-#endif
-template<class T> void _print(T t){ cerr << t;}
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fi); cerr << ","; _print(p.se); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> istream& operator>>(istream& in, vector<T> &v){for (auto& i : v) in >> i; return in;}
-template <class T> ostream& operator<<(ostream& out, vector<T> &v){for (auto& i : v) out << i << ' '; return out;}
-//--------------------------------------------------------------------------------------------------------------------------------------//
-const int mod = 1e9 + 7;
-// const int mod = 998244353;
+#include <bits/stdc++.h>
 
-void fill_res(ll id,vc<vll> &ids,vll &res)
-{
-    rf(i,25,0)
-    {
-        auto it=lower_bound(all(ids[i]),id);
-        if(it!=ids[i].end())
-        {
-            res.push_back(*it);
-            fill_res(*it+1,ids,res);
-            break;
-        }
-    }
-}
+using namespace std;
+
+#define _test     \
+    int _TEST;    \
+    cin >> _TEST; \
+    while (_TEST--)
+
 int main()
-{  
-   IOS;tie;
-   int T;
-   cin>>T;
-   while(T--)
-   {
-     ll n;
-     cin>>n;
-     string s;
-     cin>>s;
-     vc<vll> ids(26);
-     f(i,0,n)
-      ids[s[i]-'a'].pb(i);
-     if(is_sorted(all(s)))
-      cout<<0<<endl;
-     else
-     {
-      vll res;
-      fill_res(0,ids,res);
-      ll m=res.size();
-      f(i,0,m/2)
-       swap(s[res[i]],s[res[m-i-1]]);
-      debug(res)
-      if(is_sorted(all(s)))
-      {
-        ll ans=m;
-        ll pt=0;
-        f(i,1,m)
-         if(s[res[i]]!=s[res[i-1]])
-          pt=i;
-        ans-=m-pt;
-        cout<<ans<<endl;
-      }
-      else
-       cout<<-1<<endl;
-     }
-     
-   }
-    return 0;
+{
+    _test
+    {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        vector<int> v(n);
+
+        string temp = s, t;
+        reverse(temp.begin(), temp.end());
+        char ch = temp[0];
+        int idx = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (temp[i] >= ch)
+            {
+                t.push_back(ch);
+                ch = temp[i];
+                v[n - idx - 1] = 1;
+                idx = i;
+            }
+        }
+        t.push_back(ch);
+        v[n - idx - 1] = 1;
+        reverse(t.begin(), t.end());
+
+        int ans = 0;
+        for (int i = 25; i >= 0; i--)
+        {
+            if (count(t.begin(), t.end(), i + 'a') > 0)
+            {
+                ans -= count(t.begin(), t.end(), i + 'a');
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i])
+            {
+                ans++;
+                s[i] = t.back();
+                t.pop_back();
+            }
+        }
+
+        if (is_sorted(s.begin(), s.end()))
+            cout << ans << "\n";
+        else
+            cout << -1 << "\n";
+    }
 }
