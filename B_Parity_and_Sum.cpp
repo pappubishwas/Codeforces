bf41c899b7fbbf11054b758e7904cd0897428602
pappubishwas <pappuovi8@gmail.com>
2026-03-23 09:45:23 +0530

    Updated cp files

---
+++ B_Parity_and_Sum.cpp
@@ -0,0 +1,224 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define int long long
+#define INF (int)1e18
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int rndRange(int l, int r) { return RNG() % (r - l + 1) + l; }
+
+int binaryLength(int n)
+{
+    return 64 - __builtin_clzll(n);
+}
+int mod = 998244353;
+int MOD = 1e9 + 7;
+class DSU
+{
+public:
+    int n;
+    vector<int> p, rank;
+    DSU(int sz)
+    {
+        p.assign(sz, 0);
+        rank.assign(sz, 0);
+        for (int i = 0; i < sz; i++)
+            p[i] = i;
+    }
+    int find(int x)
+    {
+        if (p[x] == x)
+            return x;
+        return p[x] = find(p[x]);
+    }
+    void Unite(int x, int y)
+    {
+        int px = find(x);
+        int py = find(y);
+        if (px == py)
+            return;
+        if (rank[px] < rank[py])
+        {
+            p[px] = py;
+        }
+        else if (rank[px] > rank[py])
+        {
+            p[py] = px;
+        }
+        else
+        {
+            p[py] = px;
+            rank[px]++;
+        }
+    }
+};
+class SegmentTree
+{
+public:
+    int size;
+    vector<int> tree;
+    SegmentTree(int n)
+    {
+        size = 4 * n;
+        tree.assign(size, 0);
+    }
+    void build(int idx, int l, int r, vector<int> &a)
+    {
+        if (l == r)
+        {
+            tree[idx] = a[l];
+            return;
+        }
+        int mid = (l + r) / 2;
+        build(2 * idx + 1, l, mid, a);
+        build(2 * idx + 2, mid + 1, r, a);
+        tree[idx] = tree[2 * idx + 1] ^ tree[2 * idx + 2];
+    }
+    int query(int idx, int l, int r, int i, int val, int &ans)
+    {
+        if (l == r && i == l)
+        {
+            return val;
+        }
+        int m = (l + r) / 2;
+        if (m >= i)
+        {
+            int left = query(2 * idx + 1, l, m, i, val, ans);
+            int right = tree[2 * idx + 2];
+            if (right > left)
+            {
+                ans += (r - m);
+            }
+            return left ^ right;
+        }
+        else
+        {
+            int right = query(2 * idx + 2, m + 1, r, i, val, ans);
+            int left = tree[2 * idx + 1];
+            if (left >= right)
+            {
+                ans += (m - l + 1);
+            }
+            return left ^ right;
+        }
+    }
+};
+
+pair<int, int> get(int mid, vector<int> &pap, vector<int> &tan, int k)
+{
+    int n = pap.size();
+    int totalSum = 0, totalCount = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (pap[i] < mid)
+            continue;
+        int diff = pap[i] - mid;
+        int count = diff / tan[i] +1;
+        totalSum += (pap[i] * count - (count * (count - 1) / 2) * tan[i]);
+        totalCount += count;
+    }
+    if (totalCount > k)
+    {
+        totalSum -= (totalCount - k) * mid;
+    }
+    return {totalCount, totalSum};
+}
+
+// void solve()
+// {
+//     int n;
+//     cin>>n;
+//     vector<int> pap;
+//     int mx=0;
+//     for(int i=0;i<n;i++){
+//         int x; cin>>x;
+//         if(x%2==0)
+//         pap.push_back(x);
+//         if(x%2)
+//             mx=max(mx,x);
+//     }
+//     if(pap.size()==0 || pap.size()==n){
+//         cout<<0<<endl;
+//         return;
+//     }
+//     int ans=0;
+//     sort(pap.begin(),pap.end());
+//     for(int i=0;i<pap.size();i++){
+//         if(pap[i]<=mx){
+//             ans++;
+//             mx=pap[i]+mx;
+//         }else{
+//             int diff=pap[i]-mx;
+//             int count=(diff+pap[i]-1)/pap[i];
+//             ans+=count;
+//             ans++;
+//             mx+=count*pap[i];
+//         }
+//     }
+//     cout<<ans<<endl;
+// }
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> pap;
+    long long mx = 0; 
+    
+    for(int i = 0; i < n; i++){
+        long long x; cin >> x;
+        if(x % 2 == 0)
+            pap.push_back(x);
+        else
+            mx = max(mx, x);
+    }
+    
+    if(pap.size() == 0 || pap.size() == n){
+        cout << 0 << endl;
+        return;
+    }
+    
+    sort(pap.begin(), pap.end());
+    
+    int ans = pap.size(); 
+    
+    for(int i = 0; i < pap.size(); i++){
+        if(pap[i] < mx){
+            mx += pap[i];
+        } else {
+            ans += 1;
+            break;
+        }
+    }
+    
+    cout << ans << endl;
+}
+
+
+int32_t main()
+{
+    auto begin = chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // freopen("in",  "r", stdin);
+    // freopen("out", "w", stdout);
+
+    cin >> t;
+
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case #" << i << ": ";
+        solve();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    // cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
